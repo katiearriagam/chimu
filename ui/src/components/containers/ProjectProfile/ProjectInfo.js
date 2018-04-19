d71e1c255cf7d28a797330b0aa7a8055601b0be3
Oscar González <osdagoso@hotmail.com>
2018-04-18 21:35:40 -0500

    Implemented routing, adjusted UserInfo & ProjectInfo accordingly

--- ui/src/components/containers/ProjectProfile/ProjectInfo.js
+++ ui/src/components/containers/ProjectProfile/ProjectInfo.js
@@ -6,27 +6,26 @@ import ProjectInfoSideBar from '../../presentational/Project/ProjectInfoSideBar'
 class ProjectInfo extends Component {
   	componentDidMount(){
 		var db = firebase.firestore();
-		// Get user information from Firebase
-		var docRef = db.collection("Projects").doc(this.props.propKey);
+		// Get project information from Firebase
+		var docRef = db.collection("Projects").doc(this.props.match.params.username).collection("projects").doc(this.props.match.params.project);
 		docRef.get().then((doc) => {
 			if (doc.exists) {
 				var info = doc.data();
-				info.owner.get().then((user) => {
-					if (user.exists) {
-						this.setState({
-							keys: info.keywords,
-							name: info.name,
-							status: info.status,
-							repo_url: info.repo,
-							owner: user.id,
-							avatar_url: 'https://images.fineartamerica.com/images-medium-large/puppy-portrait-snake-jagger.jpg',
-							sdesc: info.sdesc,
-						});
-					}
-				}).catch((error) => {
-					console.log("Error getting document:", error);
+				console.log(info);
+				this.setState({
+					keys: info.keywords,
+					name: doc.id,
+					status: info.status,
+					repo_url: info.repo,
+					owner: this.props.match.params.username,
+					avatar_url: 'https://images.fineartamerica.com/images-medium-large/puppy-portrait-snake-jagger.jpg',
+					sdesc: info.sdesc,
+					valid: true,
 				});
 			} else {
+				this.setState({
+					valid: false,
+				})
 				console.log("No such document!");
 			}
 		}).catch((error) => {
@@ -37,19 +36,23 @@ class ProjectInfo extends Component {
 
 	render() {
 		if (this.state) {
-			return (
-				<div className="UserInfo">
-					<ProjectInfoSideBar 
-						avatar={this.state.avatar_url}
-						name={this.state.name}
-						status={this.state.status}
-						repo={this.state.repo_url}
-						owner={this.state.owner}
-						sdesc={this.state.sdesc}
-						keys={this.state.keys}
-					/>
-				</div>
-			);
+			if (this.state.valid) {
+				return (
+					<div className="UserInfo">
+						<ProjectInfoSideBar 
+							avatar={this.state.avatar_url}
+							name={this.state.name}
+							status={this.state.status}
+							repo={this.state.repo_url}
+							owner={this.state.owner}
+							sdesc={this.state.sdesc}
+							keys={this.state.keys}
+						/>
+					</div>
+				);
+			} else {
+				return <h1>Error 404</h1>;
+			}
 		} else {
 			return null;
 		}
